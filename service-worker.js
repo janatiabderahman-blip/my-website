2b214afcf3c72f0b3c119c163fbe6a3fc868ab7d
janatiabderahman-blip <janatiabderahman@gmail.com>
2026-02-01 19:21:24 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
 self.addEventListener("install", () => {
   console.log("FixlyHub Service Worker Installed");
 });
+
+self.addEventListener("push", function(event) {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: "assets/img/favicon.png"
+  });
+});
